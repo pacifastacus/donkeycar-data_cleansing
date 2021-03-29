06a5daba84da8f8d6711cd2fcf66e951468513f1
pacifastacus <palkovics.denes@gmail.com>
2021-03-29 19:09:17 +0200

    Added directory change feature
    -this commit maybe trashed in the future

--- classify.py
+++ classify.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import math
 import tkinter as tk
+from tkinter import filedialog
 import json
 import os.path
 
@@ -29,8 +30,9 @@ def polar2cartesian(length, angle):
 
 
 class App(tk.Frame):
-    def __init__(self, master=None, data=None):
+    def __init__(self, master=None, data=None, saveCallback=None):
         tk.Frame.__init__(self, master)
+        self.saveCallback = saveCallback
         self.frame_increment = tk.IntVar()
         self.datadir, self.data = data
         self.loaded_img = None
@@ -43,20 +45,31 @@ class App(tk.Frame):
         self.step_scale = None
         self.FRAME_COUNT = findFirstNonClassified(self.data)
         self.pack(**tk_pack_options)
+
         img_frame = tk.Frame(self)
         img_frame.pack(side=tk.TOP,**tk_pack_options)
-        ctrl_frame = tk.Frame(self)
-        ctrl_frame.pack(side=tk.BOTTOM,expand=1,fill=tk.X)
-        classify_frame = tk.Frame(ctrl_frame)
-        classify_frame.pack(side=tk.LEFT,expand=1,fill=tk.Y)
-        nav_frame = tk.Frame(ctrl_frame)
-        nav_frame.pack(side=tk.RIGHT,expand=1,fill=tk.Y)
         self.canvas = tk.Canvas(img_frame, width=200, height=200, bg="green")
         fname_label = tk.Label(img_frame, textvariable=self.img_fname, relief=tk.RAISED)
         fname_label.pack()
         self.canvas.pack()
-        # lmain = Label(img_frame)
-        # lmain.pack()
+
+        ctrl_frame = tk.Frame(self)
+        ctrl_frame.pack(side=tk.BOTTOM,expand=1,fill=tk.X)
+
+        classify_frame = tk.Frame(ctrl_frame)
+        classify_frame.pack(side=tk.LEFT,expand=1,fill=tk.Y)
+        self.class_label = tk.Label(classify_frame, textvariable=self.img_class, relief=tk.SUNKEN)
+        good_button = tk.Button(classify_frame, text="Good (1)", command=self.mark_good)
+        unmark_button = tk.Button(classify_frame, text="Unlabel (Del)", command=self.unmark)
+        bad_button = tk.Button(classify_frame, text="Bad (0)", command=self.mark_bad)
+        self.class_label.pack(side=tk.TOP, anchor=tk.CENTER)
+        unmark_button.pack(side=tk.BOTTOM)
+        good_button.pack(side=tk.RIGHT, anchor=tk.W)
+        bad_button.pack(side=tk.LEFT, anchor=tk.E)
+
+
+        nav_frame = tk.Frame(ctrl_frame)
+        nav_frame.pack(side=tk.RIGHT,expand=1,fill=tk.Y)
         prev_button = tk.Button(nav_frame, text="<<", command=self.call_prev_frame)
         next_button = tk.Button(nav_frame, text=">>", command=self.call_next_frame)
         self.step_scale = tk.Scale(nav_frame, label="step increment", orient=tk.HORIZONTAL,
@@ -64,14 +77,8 @@ class App(tk.Frame):
         self.step_scale.pack(side=tk.TOP,anchor=tk.CENTER)
         prev_button.pack(side=tk.LEFT,anchor=tk.E)
         next_button.pack(side=tk.RIGHT,anchor=tk.W)
-        self.class_label = tk.Label(classify_frame, textvariable=self.img_class, relief=tk.SUNKEN)
-        good_button = tk.Button(classify_frame, text="Good (1)", command=self.mark_good)
-        unmark_button = tk.Button(classify_frame,text="Unlabel (Del)",command=self.unmark)
-        bad_button = tk.Button(classify_frame, text="Bad (0)", command=self.mark_bad)
-        self.class_label.pack(side=tk.TOP,anchor=tk.CENTER)
-        unmark_button.pack(side=tk.BOTTOM)
-        good_button.pack(side=tk.RIGHT,anchor=tk.W)
-        bad_button.pack(side=tk.LEFT,anchor=tk.E)
+
+
         self.__show_frame()
         self.canvas.config(width=self.loaded_img.width(), height=self.loaded_img.height())
 
@@ -163,6 +170,11 @@ class App(tk.Frame):
         else:
             raise Exception()
 
+    def open_dir(self):
+        self.saveCallback()
+        foo = filedialog.askdirectory(mustexist=True, title="Select Directory")
+        app.datadir = foo
+
 
 def sort_key_fname_number(e):
     import re
@@ -193,7 +205,6 @@ def save_labeldoc(docpath, data):
         print("{},{}".format(entry[0], entry[1]), file=doc)
     doc.close()
 
-
 if __name__ == '__main__':
     import sys, os, glob
     # Get list of images
@@ -216,17 +227,21 @@ if __name__ == '__main__':
 
     data = [list(d) for d in zip(fimages_json, labels)]
     #del fimage_names, labels
+    del labels
+    def save():
+        save_labeldoc(os.path.basename(img_dir)+'_filter.csv', data)
 
     root = tk.Tk()
     root.geometry('400x300')
     root.resizable(0,0)
-    app = App(data=(img_dir,data), master=root)
+    app = App(data=(img_dir,data), master=root,saveCallback=save)
     app.master.title(__file__+" : "+img_dir)
-    #mainmenu = tk.Menu(root)
+
+    mainmenu = tk.Menu(root)
     #mainmenu.add_command(label="Save (Ctrl+S)",command=save)
-    #mainmenu.add_command(label="Load (Ctrl+O)",command=open_dialog)
-    #mainmenu.add_command(label="Quit (q)",command=app.quit)
-    #root.config(menu=mainmenu)
+    mainmenu.add_command(label="Open Directory (Ctrl+O)", command=app.open_dir)
+    mainmenu.add_command(label="Quit (q)",command=app.quit)
+    root.config(menu=mainmenu)
 
     app.bind("1", app.call_hotkey)
     app.bind("0", app.call_hotkey)
@@ -240,4 +255,5 @@ if __name__ == '__main__':
 
     app.mainloop()
 
-    save_labeldoc(os.path.basename(img_dir)+'_filter.csv', data)
+    #save_labeldoc(os.path.basename(img_dir)+'_filter.csv', data)
+    save()
