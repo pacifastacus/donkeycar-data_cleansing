eed855ae790403228abf3bd32f53d9ff1b9580a9
pacifastacus <palkovics.denes@gmail.com>
2021-03-29 22:09:51 +0200

    Implemented directory selection feature
    Now one can open another dataset directory on the run

--- classify.py
+++ classify.py
@@ -3,6 +3,7 @@ import math
 import tkinter as tk
 from tkinter import filedialog
 import json
+import csv
 import os.path
 
 import numpy as np
@@ -14,7 +15,16 @@ THROTTLE_MAX_RADIUS = 120
 LABEL_MAP = {0: "Bad",
              1: "Good",
              255: "Unlabelled"}
-tk_pack_options={"expand":1,"fill":tk.BOTH}
+tk_pack_options = {"expand": 1, "fill": tk.BOTH}
+
+
+def sort_key_fname_number(e):
+    import re
+    e = os.path.basename(e)
+    match = re.search('([^0-9]*)([0-9]+)(.*)', e)
+    num = int(match[2])
+    return num
+
 
 def findFirstNonClassified(data):
     for i, rec in enumerate(data):
@@ -22,15 +32,16 @@ def findFirstNonClassified(data):
             return i
     return 0
 
+
 def polar2cartesian(length, angle):
     angle = math.radians(angle)
-    x = length*math.cos(angle)
-    y = length*math.sin(angle)
-    return x,y
+    x = length * math.cos(angle)
+    y = length * math.sin(angle)
+    return x, y
 
 
 class App(tk.Frame):
-    def __init__(self, master=None, data=None, saveCallback=None):
+    def __init__(self, master=None, directory=None):
         tk.Frame.__init__(self, master)
         self.pack(**tk_pack_options)
         # Internal variables
@@ -42,18 +53,18 @@ class App(tk.Frame):
         # Application state vars
         self.loaded_img = None
         self.loaded_json = None
-        self.datadir, self.data = data
+        self.datadir = directory
+        self.data = self.load_labeldoc(directory + '_filter.csv')
         self.FRAME_COUNT = findFirstNonClassified(self.data)
         # End of Internal variables
 
-        self.saveCallback = saveCallback
         self.canvas = None
         self.class_label = None
         self.step_scale = None
 
         # Image view panel
         img_frame = tk.Frame(self)
-        img_frame.pack(side=tk.TOP,**tk_pack_options)
+        img_frame.pack(side=tk.TOP, **tk_pack_options)
         self.canvas = tk.Canvas(img_frame, width=200, height=200, bg="green")
         fname_label = tk.Label(img_frame, textvariable=self.img_fname, relief=tk.RAISED)
         fname_label.pack()
@@ -62,10 +73,10 @@ class App(tk.Frame):
 
         # Control panel
         ctrl_frame = tk.Frame(self)
-        ctrl_frame.pack(side=tk.BOTTOM,expand=1,fill=tk.X)
+        ctrl_frame.pack(side=tk.BOTTOM, expand=1, fill=tk.X)
         # labelling controls
         classify_frame = tk.Frame(ctrl_frame)
-        classify_frame.pack(side=tk.LEFT,expand=1,fill=tk.Y)
+        classify_frame.pack(side=tk.LEFT, expand=1, fill=tk.Y)
         self.class_label = tk.Label(classify_frame, textvariable=self.img_class, relief=tk.SUNKEN)
         good_button = tk.Button(classify_frame, text="Good (1)", command=self.mark_good)
         unmark_button = tk.Button(classify_frame, text="Unlabel (Del)", command=self.unmark)
@@ -76,22 +87,25 @@ class App(tk.Frame):
         bad_button.pack(side=tk.LEFT, anchor=tk.E)
         # navigation controls
         nav_frame = tk.Frame(ctrl_frame)
-        nav_frame.pack(side=tk.RIGHT,expand=1,fill=tk.Y)
+        nav_frame.pack(side=tk.RIGHT, expand=1, fill=tk.Y)
         prev_button = tk.Button(nav_frame, text="<<", command=self.call_prev_frame)
         next_button = tk.Button(nav_frame, text=">>", command=self.call_next_frame)
         self.step_scale = tk.Scale(nav_frame, label="step increment", orient=tk.HORIZONTAL,
-                                from_=1, to=100, resolution=1, variable=self.frame_increment)
-        self.step_scale.pack(side=tk.TOP,anchor=tk.CENTER)
-        prev_button.pack(side=tk.LEFT,anchor=tk.E)
-        next_button.pack(side=tk.RIGHT,anchor=tk.W)
+                                   from_=1, to=100, resolution=1, variable=self.frame_increment)
+        self.step_scale.pack(side=tk.TOP, anchor=tk.CENTER)
+        prev_button.pack(side=tk.LEFT, anchor=tk.E)
+        next_button.pack(side=tk.RIGHT, anchor=tk.W)
         # End of control panel
 
         # Load the first image then fit the canvas to the image
         self.__show_frame()
         self.canvas.config(width=self.loaded_img.width(), height=self.loaded_img.height())
 
+    def __del__(self):
+        self.save_labeldoc()
+
     def __load_image(self):
-        #fname = self.data[self.FRAME_COUNT][0]
+        # fname = self.data[self.FRAME_COUNT][0]
         fname = os.path.join(self.datadir, self.loaded_json["cam/image_array"])
         self.loaded_img = ImageTk.PhotoImage(Image.open(fname, "r"))
         self.img_fname.set(fname)
@@ -101,7 +115,7 @@ class App(tk.Frame):
         with open(fname) as f:
             self.loaded_json = json.load(f)
 
-    def __refresh_label_color(self,var,indx,mode):
+    def __refresh_label_color(self, var, indx, mode):
         label = self.img_class.get().lower()
         color = "red" if label == "bad" else "green" if label == "good" else "yellow"
         self.class_label.configure(bg=color)
@@ -114,12 +128,12 @@ class App(tk.Frame):
         self.canvas.create_image(0, 0, anchor='nw', image=self.loaded_img)
         x1 = 80
         y1 = 120
-        throttle = self.loaded_json["user/throttle"]*THROTTLE_MAX_RADIUS
-        angle = self.loaded_json["user/angle"]*WHEELS_TURNING_ANGLE
-        x2, y2 = polar2cartesian(throttle,angle)
-        x2,y2 = y2+x1,y1-x2
+        throttle = self.loaded_json["user/throttle"] * THROTTLE_MAX_RADIUS
+        angle = self.loaded_json["user/angle"] * WHEELS_TURNING_ANGLE
+        x2, y2 = polar2cartesian(throttle, angle)
+        x2, y2 = y2 + x1, y1 - x2
 
-        self.canvas.create_line(x1, y1, x2, y2,  fill="red", width = 2, arrow=tk.LAST)
+        self.canvas.create_line(x1, y1, x2, y2, fill="red", width=2, arrow=tk.LAST)
 
     def call_next_frame(self):
         increment = self.frame_increment.get()
@@ -178,77 +192,90 @@ class App(tk.Frame):
         else:
             raise Exception()
 
-    def open_dir(self):
-        self.saveCallback()
-        foo = filedialog.askdirectory(mustexist=True, title="Select Directory")
-        app.datadir = foo
+    #    @staticmethod
+    #    def load_labeldoc(self, docpath, fimagelist):
+    #        labels_ = [list(LABEL_MAP.keys())[list(LABEL_MAP.values()).index("Unlabelled")]] * len(fimagelist)
+    #        try:
+    #            doc = open(docpath, "r")
+    #            doc.readline()  # Skip header
+    #            for i, line in enumerate(doc):
+    #                labels_[i] = int(line.split(",")[1])
+    #            doc.close()
+    #        except FileNotFoundError:
+    #            pass
+    #        return labels_
 
+    @staticmethod
+    def load_labeldoc(docpath):
+        labels_ = []
+        json_files = []
+        try:
+            doc = open(docpath, "r")
+            reader = csv.reader(doc, delimiter=",")
+            next(reader, None)
+            for row in reader:
+                json_files.append(row[0])
+                labels_.append(int(row[1]))
+            doc.close()
+        except FileNotFoundError:
+            json_files = glob.glob(img_dir + '/record_[0-9]*.json')
+            json_files.sort(key=sort_key_fname_number)
+            labels_ = [list(LABEL_MAP.keys())[list(LABEL_MAP.values()).index("Unlabelled")]]
+            labels_ *= len(json_files)
 
-def sort_key_fname_number(e):
-    import re
-    e = os.path.basename(e)
-    match = re.search('([^0-9]*)([0-9]+)(.*)', e)
-    num = int(match[2])
-    return num
+        return [list(d) for d in zip(json_files, labels_)]
 
-
-def load_labeldoc(docpath, fimagelist):
-    import os
-    labels_ = [list(LABEL_MAP.keys())[list(LABEL_MAP.values()).index("Unlabelled")]] * len(fimagelist)
-    try:
-        doc = open(docpath, "r")
-        doc.readline()  # Skip header
-        for i, line in enumerate(doc):
-            labels_[i] = int(line.split(",")[1])
+    def save_labeldoc(self, docpath=None):
+        if not docpath:
+            docpath = self.datadir + '_filter.csv'
+        doc = open(docpath, "w")
+        print("IMAGE FILE,CLASS", file=doc)
+        for entry in self.data:
+            print("{},{}".format(entry[0], entry[1]), file=doc)
         doc.close()
-    except FileNotFoundError:
-        pass
-    return labels_
 
+    def open_dir(self):
+        newdir = filedialog.askdirectory(mustexist=True, title="Select Directory")
+        if (newdir):
 
-def save_labeldoc(docpath, data):
-    doc = open(docpath, "w")
-    print("IMAGE FILE,CLASS", file=doc)
-    for entry in data:
-        print("{},{}".format(entry[0], entry[1]), file=doc)
-    doc.close()
+            # go to the parent directory
+            dir_path, dataset = os.path.split(newdir)
+            os.chdir(dir_path)
+
+            # Save work
+            self.save_labeldoc()
+
+            # Update internal vars
+            self.datadir = dataset
+            self.data = self.load_labeldoc(self.datadir + '_filter.csv')
+            self.FRAME_COUNT = findFirstNonClassified(self.data)
+            self.__show_frame()
+
 
 if __name__ == '__main__':
     import sys, os, glob
+
     # Get list of images
     try:
-        img_dir = sys.argv[1]  #'test_data/img/'
+        img_dir = sys.argv[1]  # 'test_data/img/'
     except IndexError:
         img_dir = 'data'
-        #img_dir = input("dataset directory>")
 
-    img_dir = img_dir[:-1] if (img_dir[-1] == '/' or img_dir[-1] == '\\') else img_dir # remove the last '/' character if present
+    img_dir = img_dir[:-1] if (
+                img_dir[-1] == '/' or img_dir[-1] == '\\') else img_dir  # remove the last '/' character if present
     dir_path, img_dir = os.path.split(img_dir)
     os.chdir(dir_path)
-    #fimage_names = glob.glob(img_dir + '/*[0-9]*.jpg')
-    #fimage_names.sort(key=sort_key_fname_number)
-
-    fimages_json = glob.glob(img_dir + '/record_[0-9]*.json')
-    fimages_json.sort(key=sort_key_fname_number)
-
-    labels = load_labeldoc(img_dir+'_filter.csv', fimages_json)
-
-    data = [list(d) for d in zip(fimages_json, labels)]
-    #del fimage_names, labels
-    del labels
-    def save():
-        save_labeldoc(os.path.basename(img_dir)+'_filter.csv', data)
 
     root = tk.Tk()
     root.geometry('400x300')
-    root.resizable(0,0)
-    app = App(data=(img_dir,data), master=root,saveCallback=save)
-    app.master.title(__file__+" : "+img_dir)
+    root.resizable(0, 0)
+    app = App(master=root, directory=img_dir)
+    app.master.title('Classify.py')
 
     mainmenu = tk.Menu(root)
-    #mainmenu.add_command(label="Save (Ctrl+S)",command=save)
-    mainmenu.add_command(label="Open Directory (Ctrl+O)", command=app.open_dir)
-    mainmenu.add_command(label="Quit (q)",command=app.quit)
+    # mainmenu.add_command(label="Save (Ctrl+S)",command=save)
+    mainmenu.add_command(label="Open Directory", command=app.open_dir)
+    mainmenu.add_command(label="Quit (q)", command=app.quit)
     root.config(menu=mainmenu)
 
     app.bind("1", app.call_hotkey)
@@ -262,6 +289,4 @@ if __name__ == '__main__':
     app.focus()
 
     app.mainloop()
-
-    #save_labeldoc(os.path.basename(img_dir)+'_filter.csv', data)
-    save()
+    app.save_labeldoc()
